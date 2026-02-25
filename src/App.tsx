78650be941fc3c55ff8219cf07fda9c429e74938
Wilson Santos Designer <wilsonsantosdesigner@gmail.com>
2026-02-24 22:16:28 -0300

    Projeto estático preparado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import logo from './assets/logo.png'
 
 function App() {
 return(
-  <div>
-    <h1>PROJETO LIMPO</h1>
+  <div className='container'>
+    <img className='logo' src={logo} alt="Logo" />
+
+    <h2 className='title'>Categorias</h2>
+
+    <section className='category-area'>
+      <button className='category-button' >Motivação</button>
+      <button className='category-button' >Bem estar</button>
+    </section>
+
+    <button className='button-frase'>Gerar Frase</button>
+
+    <p className='texto-frase'>Alguma frase vai vir aqui</p>
   </div>
   )
 }

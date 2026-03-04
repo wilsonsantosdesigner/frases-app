d725e695e3b66ccb2bb08b48baf71bf09c27b1f2
Wilson Santos Designer <wilsonsantosdesigner@gmail.com>
2026-03-03 21:39:18 -0300

    Iniciando as funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
+import { useState } from 'react'
 import './App.css'
 import logo from './assets/logo.png'
 
 function App() {
-return(
+
+  const [textFrase, setTextFrase] = useState('')
+
+  const allFrases = [
+    {
+      id: 1,
+      nome: 'Motivação',
+      frase: ['Não importa o quão devagar você vá, desde que você não pare.',
+        'O sucesso é a soma de pequenos esforços repetidos dia após dia.',
+        'Acredite em si mesmo e todo o resto virá naturalmente.',
+        'O único lugar onde o sucesso vem antes do trabalho é no dicionário.',
+        'A motivação é o que te faz começar. O hábito é o que te mantém indo.']
+    },
+    {
+      id: 2,
+      nome: 'Bom dia',
+      frase: ['Bom dia! Que seu dia seja tão brilhante quanto o sol e tão alegre quanto um sorriso.',
+        'Acorde com determinação, vá para a cama com satisfação. Bom dia!',
+        'Cada manhã é uma nova oportunidade para ser feliz. Bom dia!',
+        'Que seu dia seja repleto de coisas boas e momentos felizes. Bom dia!',
+        'Bom dia! Que hoje seja o início de algo maravilhoso.']
+    },
+    {
+      id: 3,
+      nome: 'Bem estar',
+      frase: ['Cuide do seu corpo, é o único lugar que você tem para viver.',
+        'A saúde é a maior riqueza.',
+        'O bem-estar é um estado de equilíbrio entre o corpo, a mente e o espírito.',
+        'A felicidade é a melhor forma de saúde.',
+        'O autocuidado é um ato de amor próprio.']
+    }
+
+  ]
+
+return (
   <div className='container'>
     <img className='logo' src={logo} alt="Logo" />
 
@@ -15,9 +50,11 @@ return(
 
     <button className='button-frase'>Gerar Frase</button>
 
-    <p className='texto-frase'>Alguma frase vai vir aqui</p>
+    {textFrase !== '' && (
+      <p className='texto-frase'>{textFrase}</p>
+    )}
   </div>
-  )
+)
 }
 
 export default App
